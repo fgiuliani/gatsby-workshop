1a3e65368378af4a77bbd99535967d95fa8da885
Facundo Giuliani <facundo.giuliani@gmail.com>
2020-05-27 01:00:58 -0300

    add env tmpl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +29,29 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-github-api`,
+      options: {
+        token: process.env.GATSBY_GH_KEY,
+        graphQLQuery: `
+          query {
+            user(login: "fgiuliani") {
+              repositories(privacy: PUBLIC, first: 50, orderBy: {field: STARGAZERS, direction: DESC}) {
+                edges {
+                  node {
+                    id
+                    name
+                    description
+                    isPrivate
+                    url
+                  }
+                }
+              }
+            }
+          }
+        `,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Repos from "../components/repos"
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +15,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+
+    <Repos />
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
